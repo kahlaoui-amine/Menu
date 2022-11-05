e9e369ae3cf8b8800340e94534a0cfffed87950f
kahlaoui-amine <89586317+kahlaoui-amine@users.noreply.github.com>
2022-11-05 08:04:15 +0100

    first commit

---
+++ app/src/main/java/com/example/menu/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.menu;
+
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater =getMenuInflater();
+        inflater .inflate(R.menu.popup_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+        case R.id.item1:
+            Toast.makeText(this, "Nothing happened", Toast.LENGTH_SHORT).show();
+            return true;
+        case R.id.item2:
+            Toast.makeText(this, "Same here", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
